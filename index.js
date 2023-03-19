1fa566ac838537977387ab7e3a6f16f2bd993dd6
sulmi24 <a.sulmi24@gmail.com>
2023-03-19 17:05:13 +0200

    Solved : Assignment

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1);
+  }
+  static sanitize(stc) {
+    return stc.replace(/[^a-z0-9' '-]/gi, "");
+  }
+  static titleize(word) {
+    let wordsToRemove = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+
+    return word
+      .split(" ")
+      .map((ele) => {
+        return wordsToRemove.includes(ele)
+          ? word.indexOf(ele) == 0
+            ? ele.charAt(0).toUpperCase()
+            : ele
+          : ele.charAt(0).toUpperCase() + ele.slice(1);
+      })
+      .join(" ");
+  }
 }
+
+Formatter.capitalize("crocodile");
+Formatter.sanitize("ca$@#tching cold");
+Formatter.titleize("getting of giggles");
